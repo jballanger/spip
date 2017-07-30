c61f88dabef6d956618d7269cf98f012fcebcfed
jballanger <ballanger.julien@gmail.com>
2017-07-30 17:11:25 +0200

    handle error & shardDisconnect

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at:', p, 'reason:', reason);
 });
 
-global._config = require('./config.json');
+const chalk = require('chalk');
+
+try {
+	global._config = require('./config.json');
+} catch (e) {
+	console.error(chalk.red('Cannot find', chalk.bold('config.json'), '!\nPlease use', chalk.bold('config.struct.json'), 'to make your config.'));
+	process.exit(1);
+}
 
 const DiscordClient = require('./Core/DiscordClient.js');
 
@@ -11,7 +18,7 @@ var bot = new DiscordClient();
 bot.on('ready', async () => {
 	await bot.commands.init(bot);
 	bot.editStatus('online', bot.utils.game(_config.discord.game));
-	console.log(bot.user.username+'#'+bot.user.discriminator+' ready !');
+	console.log(chalk.green(`${bot.user.username}#${bot.user.discriminator} ready !`));
 });
 
 bot.on('messageCreate', (msg) => {
@@ -20,6 +27,14 @@ bot.on('messageCreate', (msg) => {
 
 bot.on('messageDelete', (msg) => {
 	bot.deleted.set(msg.id, msg);
+});
+
+bot.on('error', (err, shard) => {
+	console.error(chalk.red(`${err} on shard ${shard}`));
+});
+
+bot.on('shardDisconnect', (error, shard) => {
+	console.error(chalk.red(`Shard ${shard} disconnected :`, error.message));
 });
 
 bot.connect();
