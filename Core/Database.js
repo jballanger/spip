d359a4a23a99f6905c4bb5394ece90f0318abbb1
jballanger <ballanger.julien@gmail.com>
2017-07-30 17:50:44 +0200

    sleep and error formatting

--- Core/Database.js
+++ Core/Database.js
@@ -16,10 +16,16 @@ class Database {
 			await this.sequelize.authenticate();
 			console.log(chalk.green('Connected to database !'));
 		} catch (err) {
-			console.error(chalk.red('Failed to connect to the database, retrying in 5 seconds', err));
+			console.error(chalk.red(`Failed to connect to the database, retrying in 5 seconds..\n${err}`));
 			await this.sleep(5000);
 			return await this.authenticate();
 		}
+	}
+
+	sleep(time) {
+		return new Promise((res, rej) => {
+			setTimeout(res, time);
+		});
 	}
 }
 
