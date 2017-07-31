5d0300c291af8ddecb3c469643e3b2b73e71734f
jballanger <ballanger.julien@gmail.com>
2017-07-31 14:37:14 +0200

    manga command update

--- Commands/manga.js
+++ Commands/manga.js
@@ -1,16 +1,73 @@
 const chalk = require('chalk');
-const popura = require('popura');
+const htmlToText = require('html-to-text');
 
 exports.run = async (bot, msg, args) => {
-	const mal = popura(
-				bot.utils.decrypt(_config.myanimelist.username),
-				bot.utils.decrypt(_config.myanimelist.password)
-			);
-	var search = args.join('');
-	await mal.searchMangas(search).then((res) => {
-		console.log(res);
+	var search = args.join(' ');
+	await bot.popura.searchMangas(search).then((res) => {
+		var malUrl = 'https://myanimelist.net/manga/';
+		let manga = bot.utils.levenSort(res, search, 'title')[0];
+		if (!manga) {
+			throw  [42, 'That manga could not be found.'];
+		}
+
+		let synopsis = htmlToText.fromString(manga.synopsis, {ignoreHref: true}).replace(/\[(.+?)\]/g, '').replace(/\n/g, ' ');
+		msg.channel.createMessage({
+			embed: bot.utils.embed(
+				manga.title,
+				manga.english.length > 0 ? `(${manga.english})` : '',
+				[{
+					name: 'Chapters',
+					value: manga.chapters === 0 ? 'Unknow' : String(manga.chapters),
+					inline: true
+				},
+				{
+					name: 'Volumes',
+					value: manga.volumes === 0 ? 'Unknow' : String(manga.volumes),
+					inline: true					
+				},
+				{
+					name: 'Status',
+					value: String(manga.status),
+					inline: true
+				},
+				{
+					name: 'Score',
+					value: String(manga.score),
+					inline: true
+				},
+				{
+					name: 'Start date',
+					value: String(manga.start_date),
+					inline: true
+				},
+				{
+					name: 'End date',
+					value: manga.end_date === '0000-00-00' ? 'Unknow' : String(manga.end_date),
+					inline: true
+				},
+				{
+					name: 'Synopsis',
+					value: `${synopsis.substr(0, 500)}${synopsis.length > 500 ? '[...]' : ''}`
+				}],
+				{
+					url: `${malUrl}${manga.id}/`,
+					footer: {
+						text: 'MyAnimeList',
+						icon_url: 'https://myanimelist.cdn-dena.com/images/faviconv5.ico'
+					},
+					thumbnail: {
+						url: manga.image
+					}
+				}
+			)
+		});
 	}).catch((err) => {
-		console.error(chalk.red('Error while searching mangas with "', chalk.underline(search), `".\n${err}`));
+		if (err[0] && err[0] === 42) {
+			throw err[1];
+		} else {
+			console.error(chalk.red(`Error while searching manga "${chalk.underline(search)}".\n${err}`));
+			throw 'Something wrong happened, please contact an Admin.';
+		}
 	});
 }
 
