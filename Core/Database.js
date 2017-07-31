72a745b16bbba8ef0cf94daebd5bbbddafbb5318
jballanger <ballanger.julien@gmail.com>
2017-07-31 14:49:38 +0200

    allow to not use database if host not provided in config

--- Core/Database.js
+++ Core/Database.js
@@ -3,6 +3,7 @@ const sequelize = require('sequelize');
 
 class Database {
 	constructor() {
+		this.use = _config.database.postgres.host ? true : false;
 		this.sequelize = new sequelize(_config.database.postgres.database,
 			_config.database.postgres.user,
 			_config.database.postgres.pass, {
@@ -14,9 +15,13 @@ class Database {
 
 	async authenticate() {
 		try {
-			console.log(chalk.yellow('Connecting to database..'));
-			await this.sequelize.authenticate();
-			console.log(chalk.blue('Connected to database !'));
+			if (this.use) {
+				console.log(chalk.yellow('Connecting to database..'));
+				await this.sequelize.authenticate();
+				console.log(chalk.blue('Connected to database !'));
+			} else {
+				console.log(chalk.yellow('Not connecting to the database (Database informations missing in config).'));
+			}
 		} catch (err) {
 			console.error(chalk.red(`Failed to connect to the database, retrying in 5 seconds..\n${err}`));
 			await this.sleep(5000);
