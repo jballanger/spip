d6c553ce789fc732b6ee2f335a6c9e01311fefb5
Julien BALLANGER <jballang@e1r3p15.42.fr>
2017-08-07 13:20:34 +0200

    user reset

---
+++ Commands/reset.js
@@ -0,0 +1,14 @@
+exports.run = (bot, msg) => {
+	if (!msg.mentions[0]) throw 'Invalid user';
+	const user = msg.mentions[0];
+	bot.database.models.User.model.destroy({where: {uid: user.id}}).then((row) => {
+		if (row[0] < 1) throw `${row[0]} rows were affected`;
+	});
+}
+
+exports.info = {
+	name: 'reset',
+	description: 'Reset user\'s stats',
+	usage: 'reset <@user>',
+	level: ['Admin', 'Staff']
+}
