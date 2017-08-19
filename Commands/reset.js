ff26e87c33274d25e2c4a762415f37bb509ba67e
jballanger <ballanger.julien@gmail.com>
2017-08-19 23:26:43 +0200

    mention rewrite

--- Commands/reset.js
+++ Commands/reset.js
@@ -1,6 +1,6 @@
 exports.run = (bot, msg) => {
-	if (!msg.mentions[0]) throw 'Invalid user';
-	const user = msg.mentions[0];
+	if (!msg.mentions.users.size < 1) throw 'Invalid user';
+	const user = msg.mentions.users.first();
 	bot.database.models.User.model.destroy({where: {uid: user.id, gid: msg.channel.guild.id}}).then((row) => {
 		if (row[0] < 1) throw `${row[0]} rows were affected`;
 	});
