19559017c6251181964f25777d05bba6a77f5594
jballanger <ballanger.julien@gmail.com>
2017-09-02 23:17:45 +0200

    fix if statement, and now reply on success

--- Commands/profile/reset.js
+++ Commands/profile/reset.js
@@ -1,8 +1,9 @@
 exports.run = (bot, msg) => {
-	if (!msg.mentions.users.size < 1) throw 'Invalid user';
+	if (msg.mentions.users.size < 1) throw 'Invalid user';
 	const user = msg.mentions.users.first();
 	bot.database.models.User.model.destroy({where: {uid: user.id, gid: msg.channel.guild.id}}).then((row) => {
 		if (row[0] < 1) throw `${row[0]} rows were affected`;
+		else msg.reply(`${user.username} successfully reset.`);
 	});
 };
 
