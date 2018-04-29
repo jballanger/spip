97cb5a194dcf5d6c188b282a05d90779b7065c5f
jballanger <ballanger.julien@gmail.com>
2018-04-29 12:47:05 +0200

    Fix stat-related commands

--- src/Commands/profile/setLevel.js
+++ src/Commands/profile/setLevel.js
@@ -3,12 +3,11 @@ exports.run = (bot, msg, args) => {
   const user = msg.mentions.users.first();
   const level = parseInt(args[1], 10);
   if (!level) return msg.reply('Invalid level');
-  bot.database.models.User.model.update({
+  bot.database.models.Stats.model.update({
     level,
   }, {
     where: {
       uid: user.id,
-      gid: msg.channel.guild.id,
     },
   }).then((row) => {
     if (row[0] < 1) throw new Error(`${row[0]} rows were affected`);
