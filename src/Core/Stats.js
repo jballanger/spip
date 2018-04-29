f3617ce57d4197ef7acd07e7e18eee3715be705b
jballanger <ballanger.julien@gmail.com>
2018-04-29 12:40:47 +0200

    Fix updateStats and updateLadder methods

--- src/Core/Stats.js
+++ src/Core/Stats.js
@@ -19,49 +19,44 @@ class Stats {
     return Math.round((exp / this.constructor.formula(level + 1)) * 100);
   }
 
-  updateStats(msg) {
+  async updateStats(msg) {
     if (this.users[`${msg.channel.guild.id}${msg.author.id}`]) return;
     if (msg.content.length < 3) return;
-    this.client.database.getUser(msg.author, msg.channel.guild.id).then((user) => {
-      let exp = parseInt(user.exp, 10);
-      let points = parseInt(user.points, 10);
-      let level = parseInt(user.level, 10);
-
-      exp += this.client.utils.randomNumber(4, 9);
-      exp += (msg.mentions.channels.size + msg.mentions.roles.size + msg.mentions.users.size) * 2;
-
-      const nextLevelExp = this.constructor.formula(level + 1);
-      if (exp >= nextLevelExp) {
-        const plus = exp - nextLevelExp;
-        exp = 0;
-        if (plus > 0) exp = plus;
-        level += 1;
-        points += this.client.utils.randomNumber(level, level * 2);
-        this.client.database.models.User.model.update({
-          level,
-          exp,
-          points,
-        }, {
-          where: {
-            uid: user.uid,
-            gid: msg.channel.guild.id,
-          },
-        }).then((row) => {
-          if (row[0] < 1) throw new Error(`${row[0]} rows were affected`);
-        });
-      } else {
-        this.client.database.models.User.model.update({
-          exp,
-        }, {
-          where: {
-            uid: user.uid,
-            gid: msg.channel.guild.id,
-          },
-        }).then((row) => {
-          if (row[0] < 1) throw new Error(`${row[0]} rows were affected`);
-        });
-      }
-    });
+    const user = await this.client.database.getUserStats(msg.author.id);
+    let exp = parseInt(user.exp, 10);
+    let points = parseInt(user.points, 10);
+    let level = parseInt(user.level, 10);
+    exp += this.client.utils.randomNumber(4, 9);
+    exp += (msg.mentions.channels.size + msg.mentions.roles.size + msg.mentions.users.size) * 2;
+    const nextLevelExp = this.constructor.formula(level + 1);
+    if (exp >= nextLevelExp) {
+      const plus = exp - nextLevelExp;
+      exp = 0;
+      if (plus > 0) exp = plus;
+      level += 1;
+      points += this.client.utils.randomNumber(level, level * 2);
+      this.client.database.models.Stats.model.update({
+        level,
+        exp,
+        points,
+      }, {
+        where: {
+          uid: user.uid,
+        },
+      }).then((row) => {
+        if (row[0] < 1) throw new Error(`${row[0]} rows were affected for uid ${user.uid}`);
+      });
+    } else {
+      this.client.database.models.Stats.model.update({
+        exp,
+      }, {
+        where: {
+          uid: user.uid,
+        },
+      }).then((row) => {
+        if (row[0] < 1) throw new Error(`${row[0]} rows were affected for uid ${user.uid}`);
+      });
+    }
     this.users[`${msg.channel.guild.id}${msg.author.id}`] = new Date();
   }
 
@@ -81,27 +76,25 @@ class Stats {
     if (!this.client.database.use) {
       return (console.log(chalk.yellow('Not updating ladder (Database not used)')));
     }
-    this.channels.forEach((channel) => {
-      this.client.database.getAllUsers(channel.guild.id).then(async (users) => {
-        await users.sort(this.constructor.sortLadder);
-        await users.map((u, i) => this.client.database.updateRank(u.uid, i + 1));
-        const next = new Date();
-        next.setMinutes(next.getMinutes() + 30);
-        let ladderContent = '```xl\n';
-        if (users.length < 1) ladderContent += '\n';
-        await users.forEach((user, i) => {
-          const percent = this.getExpPercent(user.level, user.exp);
-          ladderContent += `#${i + 1} \u27A4 ${user.username} - Level ${user.level} (${percent}%)\n`;
-        });
-        ladderContent += '```\n';
-        ladderContent += `Next update at ${this.client.utils.getHours(next)}:${this.client.utils.getMinutes(next)}`;
-        channel.fetchMessages({ limit: 1 }).then((messages) => {
-          const message = messages.first();
-          if (message && message.author.id === this.client.user.id) {
-            message.edit(ladderContent);
-          } else channel.send(ladderContent);
-        });
+    this.channels.forEach(async (channel) => {
+      const channelUsers = await this.client.database.getAllUsers(channel.guild.id);
+      const channelUsersUid = channelUsers.map(u => u = u.uid);
+      const usersStat = await this.client.database.getAllUsersStat(channelUsersUid);
+      usersStat.sort(this.constructor.sortLadder);
+      usersStat.map((u, i) => this.client.database.updateRank(u.uid, i + 1));
+      const next = new Date();
+      next.setMinutes(next.getMinutes() + 30);
+      let ladderContent = '```xl\n';
+      await usersStat.forEach((stats, i) => {
+        const user = channelUsers.filter(u => u.dataValues.uid === stats.uid)[0];
+        const percent = this.getExpPercent(stats.level, stats.exp);
+        ladderContent += `#${i + 1} \u27A4 ${user.dataValues.username} - Level ${stats.level} (${percent}%)\n`;
       });
+      ladderContent += '```\n';
+      ladderContent += `Next update at ${this.client.utils.getHours(next)}:${this.client.utils.getMinutes(next)}`;
+      const message = (await channel.fetchMessages({ limit: 1 })).first();
+      if (message && message.author.id === this.client.user.id) message.edit(ladderContent);
+      else channel.send(ladderContent);
     });
     return (console.log(chalk.blue('Ladder updated for every channels !')));
   }
