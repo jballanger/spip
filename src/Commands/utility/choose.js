31b4ab23978a7cd4d82cbf66900c42de2729b956
jballanger <ballanger.julien@gmail.com>
2018-04-30 21:47:53 +0200

    Fix choose command Return with error if no choice are provided and keep spaces in choices

--- src/Commands/utility/choose.js
+++ src/Commands/utility/choose.js
@@ -1,5 +1,6 @@
 exports.run = async (bot, msg, args) => {
-  const choices = args.join('').split('|');
+  if (args.length < 1) return msg.reply('Please provide at least one choice.');
+  const choices = args.join(' ').split('|');
   const n = bot.utils.randomNumber(0, choices.length);
   await msg.channel.send(choices[n].trim());
 };
