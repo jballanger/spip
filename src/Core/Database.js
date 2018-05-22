01ac837fa4ae0a10867b130cf5cf60a5aee039a9
jballanger <ballanger.julien@gmail.com>
2018-05-22 21:12:57 +0200

    Remove useless methods, reduce models depth

--- src/Core/Database.js
+++ src/Core/Database.js
@@ -39,64 +39,11 @@ class Database {
     }
   }
 
-  initUserStats(uid) {
-    return new Promise(async (resolve) => {
-      const stats = await this.models.Stats.model.create({ uid });
-      resolve(stats);
-    });
-  }
-
-  async getUser(user, gid) {
-    return new Promise((resolve) => {
-      this.models.User.model.findOrCreate({
-        where: {
-          gid,
-          uid: user.id,
-        },
-      }).spread(async (u, created) => {
-        if (created) await this.initUserStats(user.id);
-        resolve(u.dataValues);
-      });
-    });
-  }
-
-  async getUserStats(uid) {
-    return new Promise(async (resolve) => {
-      const userStats = await this.models.Stats.model.findOne({ where: { uid } });
-      resolve(userStats.dataValues);
-    });
-  }
-
-  getAllUsers(gid) {
-    return new Promise((resolve) => {
-      resolve(this.models.User.model.findAll({
-        where: { gid },
-      }));
-    });
-  }
-
-  getAllUsersStat(uids) {
-    return new Promise((resolve) => {
-      resolve(this.models.Stats.model.findAll({
-        where: { uid: uids },
-      }));
-    });
-  }
-
-  updateRank(uid, rank) {
-    this.models.Stats.model.update(
-      { rank },
-      { where: { uid } },
-    ).then((row) => {
-      if (row[0] < 1) throw new Error(`${row[0]} rows were affected`);
-    });
-  }
-
   async loadModels() {
     this.models = {};
     Object.keys(models).forEach((model) => {
-      this.models[model] = new models[model](this.sequelize);
-      this.models[model].model.sync();
+      this.models[model] = new models[model](this.sequelize).model;
+      this.models[model].sync();
     });
   }
 
